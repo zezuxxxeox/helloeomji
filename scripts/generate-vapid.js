1d8b0bb3e4a784bbf08aa23758e558e4161b1554
zezuxxxeox <zezun2618@gmail.com>
2026-05-04 10:28:14 +0900

    refactor: remove web push notification implementation and related files

--- scripts/generate-vapid.js
+++
@@ -1,6 +0,0 @@
-import webPush from "web-push";
-
-const keys = webPush.generateVAPIDKeys();
-
-console.log("VAPID_PUBLIC_KEY=", keys.publicKey);
-console.log("VAPID_PRIVATE_KEY=", keys.privateKey);
